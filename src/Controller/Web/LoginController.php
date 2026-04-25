914821aa88bec2efd60d661a8ec508facbd80342
Konstantin Naumov <naumov-web@yandex.ru>
2026-04-26 01:44:23 +0400

    VP-2: Admin login. Request

---
+++ src/Controller/Web/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('login/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+}
